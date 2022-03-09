674059568f7f9e1870dcd2f621c1432461c8213b
Ahmad Fadhil <ahmadfadhil990@gmail.com>
2022-03-09 15:22:25 +0700

    menu_nama

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -32,7 +32,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mx-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                 </svg>
-                                <span>Sertifikat</span>
+                                <span>Daftar Permohonan</span>
                             </div>
                         </h1>
                     </x-jet-nav-link>
@@ -156,7 +156,7 @@
                 <!-- Account Management -->
 
                 <x-jet-responsive-nav-link href="{{ route('certificate') }}" :active="request()->routeIs('certificate')">
-                    <span class="text-orange-400 font-bold">Sertifikat</span>
+                    <span class="text-orange-400 font-bold">Daftar Permohonan</span>
                 </x-jet-responsive-nav-link>
 
                 <x-jet-responsive-nav-link href="{{ route('referensi') }}" :active="request()->routeIs('referensi')">
